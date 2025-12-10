9b550051c58154c32ec2c9859f4b05fefc49abcd
Hyago Oliveira <hyago.oliveira@1mbitshorde.com>
2025-12-10 18:31:29 -0300

    Add IRespawnable

---
+++ Runtime/Respawns/IRespawnable.cs
@@ -0,0 +1,10 @@
+namespace OneM.GameplaySystem
+{
+    /// <summary>
+    /// Interface used on objects able to respawn.
+    /// </summary>
+    public interface IRespawnable
+    {
+        void Respawn();
+    }
+}
